da116ef5839e565f85060b6ee81e45db93a013d4
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-06-05 11:28:28 -0400

    js created attributes to set a certain color based on the current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,17 @@ const saveBtn = $(".saveBtn");
 const currentTime = moment().format("HH");
 const currentTimeInt = parseInt(currentTime);
 
+// data attributes for the jquery function to set a certain color based on the current time
+$('#9Time').attr('timeInfo', moment('9:00 am', 'h:mm a').format('HH'));
+$('#10Time').attr('timeInfo', moment('10:00 am', 'h:mm a').format('HH'));
+$('#11Time').attr('timeInfo', moment('11:00 am', 'h:mm a').format('HH'));
+$('#12Time').attr('timeInfo', moment('12:00 pm', 'h:mm a').format('HH'));
+$('#1Time').attr('timeInfo', moment('1:00 pm', 'h:mm a').format('HH'));
+$('#2Time').attr('timeInfo', moment('2:00 pm', 'h:mm a').format('HH'));
+$('#3Time').attr('timeInfo', moment('3:00 pm', 'h:mm a').format('HH'));
+$('#4Time').attr('timeInfo', moment('4:00 pm', 'h:mm a').format('HH'));
+$('#5Time').attr('timeInfo', moment('5:00 pm', 'h:mm a').format('HH'));
+
 // JQuery Begins
 $(document).ready(function () {
 
@@ -13,4 +24,12 @@ $(document).ready(function () {
         $("#currentDay").html(moment().format('MMMM Do YYYY, h:mm a'));
     }
     setInterval(newDate, 1000);
+
+    // function that operates the saveBtn and triggers data to be stored in local storage
+    saveBtn.on("click", function () {
+        var hour = $(this).attr("data-time"); // reference to the html doc data-time
+        var input = $();
+
+        localStorage.setItem(hour, input); // saves input into local storage
+    });
 });
